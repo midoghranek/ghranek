3d866ff0f437ef5709a5e1471ad484746d219231
midoghranek <midoghranek@hotmail.com>
2019-09-26 05:06:16 +0200

    v1.0.1

--- src/index.js
+++ src/index.js
@@ -3,5 +3,5 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("app"));
 serviceWorker.register();
