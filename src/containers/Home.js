4798933f03723d97b2374cd6de6c5eaa74d3d81d
midoghranek <midoghranek@hotmail.com>
2019-12-03 19:22:25 +0200

    v4.0.4

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 
+import { BrowserRouter as Router, Link } from "react-router-dom";
+
 // Assets
 import CV from "../assets/files/GharaniqCV.pdf";
 
@@ -23,30 +25,32 @@ class Home extends Component {
 
   render() {
     return (
-      <div className="home-contents">
-        <div className="path" />
-        <div className="right-art" />
-        <div className="box">
-          <div className="inner">
-            <h1>{profile.name}</h1>
-            <span className="line"></span>
-            <p>
-              Hello, My name is Muhammad Abu 'l-Gharaniq, I'm working as a full
-              stack web developer, I'm designing and developing Mobile and Web
-              Apps.
-            </p>
-            <div className="buttons">
-              <a target="blank" className="yellow button" href={CV}>
-                Download CV
-              </a>
-              <a className="white button" href="/contact">
-                Hire me
-              </a>
+      <Router>
+        <div className="home-contents">
+          <div className="path" />
+          <div className="right-art" />
+          <div className="box">
+            <div className="inner">
+              <h1>{profile.name}</h1>
+              <span className="line"></span>
+              <p>
+                Hello, My name is Muhammad Abu 'l-Gharaniq, I'm working as a
+                full stack web developer, I'm designing and developing Mobile
+                and Web Apps.
+              </p>
+              <div className="buttons">
+                <a target="blank" className="yellow button" href={CV}>
+                  Download CV
+                </a>
+                <Link className="white button" to="/contact">
+                  Hire me
+                </Link>
+              </div>
             </div>
           </div>
+          <Social />
         </div>
-        <Social />
-      </div>
+      </Router>
     );
   }
 }
